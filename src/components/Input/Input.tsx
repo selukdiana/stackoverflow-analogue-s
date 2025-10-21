44d47356b3676594482b6882eba89ae8ee41cac7
Diana Selyuk <selukdiana@gmail.com>
2025-10-22 00:11:00 +0300

    feat: update Input component props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react';
+import { type ChangeEvent } from 'react';
 
 import styles from './Input.module.scss';
 
@@ -6,9 +6,10 @@ interface InputProps {
   label: string;
   type: 'text' | 'password' | 'email';
   name: string;
+  value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
-export const Input = ({ label, type, onChange, name }: InputProps) => {
+export const Input = ({ label, type, onChange, name, value }: InputProps) => {
   return (
     <div className={styles.container}>
       <label className={styles.label}>{label}</label>
@@ -17,6 +18,7 @@ export const Input = ({ label, type, onChange, name }: InputProps) => {
         className={styles.input}
         onChange={onChange}
         name={name}
+        value={value}
       />
     </div>
   );
