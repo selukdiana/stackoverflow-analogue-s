d66d7bf200e76a643a1442fc03b4bca39dc25dff
Diana Selyuk <selukdiana@gmail.com>
2025-10-23 14:53:02 +0300

    fix: fix btn and sidepan styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import { type MouseEvent } from 'react';
 
 import styles from './Button.module.scss';
 
 interface ButtonProps {
   children: string;
+  handleClick?: (e: MouseEvent<HTMLButtonElement>) => void;
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, handleClick }: ButtonProps) => {
   return (
-    <button className={styles.button} type="submit">
+    <button
+      className={styles.button}
+      type={handleClick ? 'button' : 'submit'}
+      onClick={handleClick}
+    >
       {children}
     </button>
   );
