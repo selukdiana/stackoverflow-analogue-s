e65aa8af36d1287959de3b09fab5fa7a101ea6b8
Diana Selyuk <selukdiana@gmail.com>
2025-10-28 15:51:06 +0300

    feat: add vercel proxy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://codelang.vercel.app/api/';
+// const BASE_URL = 'https://codelang.vercel.app/api/';
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: '/api/',
 });
 
 export default api;
